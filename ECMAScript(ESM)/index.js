d9a6cea7b327f262a73a27696eb9cc00ff3d37f2
ROSTAND LELE <rostoinfocus@gmail.com>
2025-02-16 09:34:35 +0100

    type module commonjs

--- ECMAScript(ESM)/index.js
+++ ECMAScript(ESM)/index.js
@@ -1,5 +1,5 @@
 // Importation des fonctions sum et prod depuis le module mathLib.mjs
-import { sum, prod } from "./mathLib.mjs";
+import { prod, sum } from "./mathLib.mjs";
 
 // Déclaration de deux nombres à utiliser dans les opérations
 const nbr1 = 2;  // Premier nombre
@@ -10,3 +10,8 @@ console.log(`${nbr1} + ${nbr2} = ${sum(nbr1, nbr2)}`);
 
 // Affichage du produit des deux nombres dans la console
 console.log(`${nbr1} x ${nbr2} = ${prod(nbr1, nbr2)}`);
+
+// Explication des Commentaires
+// Importation : Le code commence par importer des fonctions spécifiques (sum et prod) d'un fichier externe nommé mathLib.mjs, qui contient probablement des définitions pour effectuer des opérations mathématiques.
+// Déclaration des Nombres : Deux constantes (nbr1 et nbr2) sont définies pour stocker les valeurs numériques sur lesquelles les opérations seront effectuées.
+// Affichage des Résultats : Les résultats des opérations (somme et produit) sont calculés en appelant les fonctions importées et sont affichés dans la console avec un formatage pour une meilleure lisibilité.
--- commonJS/index.js
+++ commonJS/index.js
@@ -1,6 +1,17 @@
-let { SOMME, PRODUIT } = require("./mathLib.cjs")
+// Importation des fonctions SOMME et PRODUIT depuis le module mathLib.cjs
+let { SOMME, PRODUIT } = require("./mathLib.cjs");
 
-const nbr1 = 2;
-const nbr2 = 3;
-console.log(`${nbr1} + ${nbr2} =  ${SOMME(nbr1, nbr2)}`);
-console.log(`${nbr1} x ${nbr2} =  ${PRODUIT(nbr1, nbr2)}`);
+// Déclaration de deux nombres à utiliser dans les opérations
+const nbr1 = 2;  // Premier nombre
+const nbr2 = 3;  // Deuxième nombre
+
+// Affichage de la somme des deux nombres dans la console
+console.log(`${nbr1} + ${nbr2} = ${SOMME(nbr1, nbr2)}`);
+
+// Affichage du produit des deux nombres dans la console
+console.log(`${nbr1} x ${nbr2} = ${PRODUIT(nbr1, nbr2)}`);
+
+// Explication des Commentaires
+// Importation : Le code commence par importer des fonctions spécifiques (SOMME et PRODUIT) d'un fichier externe nommé mathLib.cjs, qui est probablement un module CommonJS contenant des définitions pour effectuer des opérations mathématiques.
+// Déclaration des Nombres : Deux constantes (nbr1 et nbr2) sont définies pour stocker les valeurs numériques sur lesquelles les opérations seront effectuées.
+// Affichage des Résultats : Les résultats des opérations (somme et produit) sont calculés en appelant les fonctions importées et sont affichés dans la console avec un formatage pour une meilleure lisibilité.
