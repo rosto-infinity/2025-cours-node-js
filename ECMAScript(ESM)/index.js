a2ce5165df1382594c3c74a31c44c1cc5db98ab5
ROSTAND LELE <rostoinfocus@gmail.com>
2025-02-16 09:32:36 +0100

    type module

--- ECMAScript(ESM)/index.js
+++ ECMAScript(ESM)/index.js
@@ -1,6 +1,12 @@
-import  { sum, prod }  from "./mathLib.mjs"
+// Importation des fonctions sum et prod depuis le module mathLib.mjs
+import { sum, prod } from "./mathLib.mjs";
 
-const nbr1 = 2;
-const nbr2 = 55;
-console.log(`${nbr1} + ${nbr2} =  ${sum(nbr1, nbr2)}`);
-console.log(`${nbr1} x ${nbr2} =  ${prod(nbr1, nbr2)}`);
+// Déclaration de deux nombres à utiliser dans les opérations
+const nbr1 = 2;  // Premier nombre
+const nbr2 = 55; // Deuxième nombre
+
+// Affichage de la somme des deux nombres dans la console
+console.log(`${nbr1} + ${nbr2} = ${sum(nbr1, nbr2)}`);
+
+// Affichage du produit des deux nombres dans la console
+console.log(`${nbr1} x ${nbr2} = ${prod(nbr1, nbr2)}`);
