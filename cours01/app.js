3983da5487d6d19f46829cf36c45f08220970cdb
ROSTAND LELE <rostoinfocus@gmail.com>
2025-02-16 09:59:38 +0100

    Avec les Promises

--- cours01/app.js
+++ cours01/app.js
@@ -16,7 +16,7 @@ lireFichier('exemple.txt');
 // a. Avec async/await et try/catch
 async function fetchData() {
   try {
-    const response = await fetch('https://api.example.com/data');
+    const response = await fetch('https://api.blablagues.net/?rub=blagues');
     const data = await response.json();
     console.log(data);
   } catch (error) {
@@ -25,3 +25,13 @@ async function fetchData() {
 }
 
 fetchData();
+
+
+fetch('https://api.blablagues.net/?rub=blagues')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Erreur lors de la récupération des données:', error);
+  });
