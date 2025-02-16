5136ef73f7cc8e8d6b40733bd688b631c39b4a78
ROSTAND LELE <rostoinfocus@gmail.com>
2025-02-16 09:30:15 +0100

    Initial commit

---
+++ ECMAScript(ESM)/index.js
@@ -0,0 +1,6 @@
+import  { sum, prod }  from "./mathLib.mjs"
+
+const nbr1 = 2;
+const nbr2 = 55;
+console.log(`${nbr1} + ${nbr2} =  ${sum(nbr1, nbr2)}`);
+console.log(`${nbr1} x ${nbr2} =  ${prod(nbr1, nbr2)}`);
---
+++ ECMAScript(ESM)/mathLib.mjs
@@ -0,0 +1,9 @@
+const sum = (a, b) => a + b;
+const prod = (a, b) => a * b;
+
+export {sum, prod}
+
+
+// function som(a, b){
+//   return a + b;
+// }
