2fa525e9dba753cbc9977d634357ea646b8c9381
ROSTAND LELE <rostoinfocus@gmail.com>
2025-02-17 04:19:42 +0100

    Comment faire un serveur avec Node js

---
+++ module-06/backend/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const port = 5000;
+
+const app = express();
+
+app.get("/post", (req, res) => {
+  res.json({message : "Voici les données !"})
+})
+// Lancer le serveur
+app.listen(port, () => console.log("Le serveur a démarré au port  " + port));
