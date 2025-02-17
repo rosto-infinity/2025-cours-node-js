a63a3791d2731332bb6e8883106ff3bd3ab88714
ROSTAND LELE <rostoinfocus@gmail.com>
2025-02-17 05:04:21 +0100

    postman et isomnia

--- module-06/backend/server.js
+++ module-06/backend/server.js
@@ -4,7 +4,7 @@ const port = 5000;
 const app = express();
 
 app.get("/post", (req, res) => {
-  res.json({message : "Voici les données !"})
+  res.json({message : "Voici les données envoyer ok !"})
 })
 // Lancer le serveur
 app.listen(port, () => console.log("Le serveur a démarré au port  " + port));
